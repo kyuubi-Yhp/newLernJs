af942fcbaa1c27256fcb8d1eb69a8ee4ff173dcc
kyuubi-Yhp <south01village@gmail.com>
2025-12-09 02:02:45 +0300

    созданна ветка и запушино задание

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+const title = 1
+const csreen = 1
+const scrinPrise = 1
+const rollback = 1
+const fullPrise = 1
+const adaptive = 1
+
+console.log(121123)
+
+let namePrompt = prompt('Введите ваше имя');
+
+while (
+  namePrompt === null ||             // Нажали "Отмена"
+  namePrompt.trim() === '' ||        // Пусто или пробелы
+  /\d/.test(namePrompt)              // Есть цифры
+) {
+  alert('Имя не может быть пустым, состоять из пробелов или содержать цифры.');
+  namePrompt = prompt('Введите ваше имя');
+}
+alert(`Отлично, ${namePrompt}!`);
